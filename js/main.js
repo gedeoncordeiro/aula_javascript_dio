6ed34cb5bf7f5a75c8a723d08d5e83672270aff3
Gedeon Cordeiro <gedeon.donn@gmail.com>
2022-03-07 00:42:35 -0300

    m

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// var nome = prompt("Digite seu nome:")
+// var idade = prompt("Digite sua idade:")
+
+// if(idade >= 18){
+//     alert("Bem vindo " + nome + "!");
+// }else{
+//     alert("Por favor saida do site " + nome + "!");
+// };
+
+// var sites = ["globo.com", "google.com", "youtube.com"];
+
+// // lista.pop();
+// console.log(sites.reverse());
+// console.log(sites.toString());
+// console.log(sites.join(" www."));
+
+// var fruta = {
+//     nome: "Maçã",
+//     cor: "Vermelha"
+// };
+
+// console.log(fruta.cor); 
+
+// var idade = prompt("Digite sua idade:")
+// if(idade >= 18){
+//     alert("Maior de idade")
+// }else{
+//     alert("Menor de idade")
+// }
+
+var d = new Date();
+alert(d.getDay());
